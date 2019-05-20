59e38ceaaa70a60419ce2ede6b40dc55d76d623c
selwill <selwyn.williams.jr@gmail.com>
2019-05-20 16:38:45 -0500

    working on a sign in and registration page

---
+++ RegistrationLogin/MyConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class MyConnection {
+
+
+    // create a function to connect with mysql database
+
+    public static Connection getConnection(){
+
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/java_login_register", "root", "");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return con;
+    }
+
+}
